851411aa5486ed291a11c555e7f9f523b670f205
davidmeimoun <david.meimoun@hotmail.fr>
2018-01-26 14:37:15 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
 
-sudo curl -Ss https://getcomposer.org/installer | php
-sudo mv composer.phar /usr/bin/composer
-sudo apt-get update
-
 sudo apt-get install git
 sudo apt-get update
 cd /var/www/html
 sudo git clone https://github.com/davidmeimoun/PSI
 
 export COMPOSER_ALLOW_SUPERUSER=1
-cd PSI
-sudo composer install
+mv -a vendor/ PSI/
